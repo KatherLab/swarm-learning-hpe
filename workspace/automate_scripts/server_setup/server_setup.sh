49c396ad0bf10fe8036e0835f3017a628773133c
Jeff <jiefu.zhu@tu-dresden.de>
2023-01-25 15:17:06 +0100

    update server setup

--- workspace/automate_scripts/server_setup/server_setup.sh
+++ workspace/automate_scripts/server_setup/server_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eux
 
+pip install gdown
+gdown --folder https://drive.google.com/drive/folders/1Y6DR-7Gp1uWsqQ1CDZGb6cxMD34-NOtG  # for folders
+
 cd ./pls-9.12/UNIX
 chmod a+x setup.bin
 sudo ./setup.bin
