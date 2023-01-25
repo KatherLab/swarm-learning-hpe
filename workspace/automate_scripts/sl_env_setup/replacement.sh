8c4025d39480fb082658e72aaa6678f055180d25
Jeff <jiefu.zhu@tu-dresden.de>
2023-01-25 17:57:23 +0100

    update auto replacement

--- workspace/automate_scripts/sl_env_setup/replacement.sh
+++ workspace/automate_scripts/sl_env_setup/replacement.sh
@@ -31,6 +31,10 @@ then
    help
 fi
 
+cp workspace/"$workspace"/swop/swop_profile.yaml workspace/"$workspace"/swop/swop_profile_"$ip_addr".yaml
+cp workspace/"$workspace"/swci/swci_init workspace/"$workspace"/swci/swci_init_"$ip_addr"
+cp workspace/"$workspace"/swci/taskdefs/*.yaml workspace/"$workspace"/swci/taskdefs/*_"$ip_addr".yaml
+
 sed -i "s+<CURRENT-PATH>+$(pwd)+g" workspace/"$workspace"/swop/swop_profile.yaml workspace/"$workspace"/swci/taskdefs/*.yaml workspace/"$workspace"/swci/swci_init
 sed -i "s+<SN-IPADDRESS>+$(sentinal_host)+g" workspace/"$workspace"/swop/swop_profile.yaml workspace/"$workspace"/swci/taskdefs/*.yaml workspace/"$workspace"/swci/swci_init
 sed -i "s+<HOST-IPADDRESS>+$(ip_addr)+g" workspace/"$workspace"/swop/swop_profile.yaml workspace/"$workspace"/swci/taskdefs/*.yaml workspace/"$workspace"/swci/swci_init
