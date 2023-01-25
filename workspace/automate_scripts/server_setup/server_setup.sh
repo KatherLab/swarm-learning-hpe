c327ebe9b842a5ec474b08aa0f56116dc2105d63
Jeff <jiefu.zhu@tu-dresden.de>
2023-01-25 15:32:13 +0100

    update server setup

--- workspace/automate_scripts/server_setup/server_setup.sh
+++ workspace/automate_scripts/server_setup/server_setup.sh
@@ -3,7 +3,15 @@ set -eux
 
 sudo apt install python3-pip -y
 sudo pip install gdown
-gdown --folder https://drive.google.com/drive/folders/166t3FPPtI25CQdNXkYtfX7thmXJcnOuO?usp=share_link  # for folders
+
+DIR="/apls-9.12/"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "APLS already installed"
+else
+  gdown --folder https://drive.google.com/drive/folders/166t3FPPtI25CQdNXkYtfX7thmXJcnOuO?usp=share_link  # for folders
+
+fi
 
 cd ./apls-9.12/UNIX
 chmod a+x setup.bin
