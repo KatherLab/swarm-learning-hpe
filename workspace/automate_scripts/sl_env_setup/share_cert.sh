675074021f5342daa757a6e8cc2eb837b9283d33
Jeff <jiefu.zhu@tu-dresden.de>
2023-01-25 17:00:24 +0100

    update license install setup

--- workspace/automate_scripts/sl_env_setup/share_cert.sh
+++ workspace/automate_scripts/sl_env_setup/share_cert.sh
@@ -5,13 +5,31 @@ ip_addr=$(hostname -I | awk '{print $1}')
 script_name=$(basename "${0}")
 script_dir=$(realpath $(dirname "${0}"))
 
+
+# Help function
+help()
+{
+   echo ""
+   echo "Ask jeff how to use the damn script"
+   echo ""
+   exit 1
+}
+
 # Process command options
 while getopts "t:e:" opt
 do
    case "$opt" in
       t ) target_host="$OPTARG" ;;
       e ) workspace="$OPTARG" ;;
+      h ) help ;;
+      ? ) help ;;
    esac
 done
 
+# Checks
+if [ -z "$target_host" ] || [ -z "$workspace" ]
+then
+   echo "Some or all of the parameters are empty";
+   help
+fi
 sudo scp swarm@"$target_host":/opt/hpe/swarm-learning-hpe/workspace/"$workspace"/cert/ca/capath/ca-"$target_host"-cert.pem workspace/"$workspace"/cert/ca/capath
