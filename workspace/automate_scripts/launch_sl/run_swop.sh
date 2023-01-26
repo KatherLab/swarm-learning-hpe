f5dba271cc8bb5882842ee9aa27bbe4aef11ac5b
Jeff <jiefu.zhu@tu-dresden.de>
2023-01-26 15:53:26 +0100

    update create docker net and volume

--- workspace/automate_scripts/launch_sl/run_swop.sh
+++ workspace/automate_scripts/launch_sl/run_swop.sh
@@ -28,7 +28,7 @@ done
 
 sudo $script_dir/../../swarm_learning_scripts/run-swop -it --rm --name=swop"$ip_addr" \
 --network=host-"$ip_addr"-net --usr-dir=workspace/"$workspace"/swop \
---profile-file-name=swop"$ip_addr"_profile.yaml \
+--profile-file-name=swop-"$ip_addr"_profile.yaml \
 --key=workspace/"$workspace"/cert/swop-"$ip_addr"-key.pem \
 --cert=workspace/"$workspace"/cert/swop-"$ip_addr"-cert.pem \
 --capath=workspace/"$workspace"/cert/ca/capath \
