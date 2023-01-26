c43b15a53fd6c176372cf8483428519127b52722
Jeff <jiefu.zhu@tu-dresden.de>
2023-01-26 15:14:49 +0100

    update create docker net and volume

--- workspace/automate_scripts/sl_env_setup/create_sldocker_wheel.sh
+++ workspace/automate_scripts/sl_env_setup/create_sldocker_wheel.sh
@@ -21,8 +21,8 @@ do
    esac
 done
 
-#if sudo docker volume list | grep -q 'sl-cli-lib'; then sudo docker volume rm sl-cli-lib; fi
-#sudo docker volume create sl-cli-lib
+if sudo docker volume list | grep -q 'sl-cli-lib'; then sudo docker volume rm sl-cli-lib; fi
+sudo docker volume create sl-cli-lib
 sudo docker container create --name helper -v sl-cli-lib:/data hello-world
 sudo docker cp -L $script_dir/swarmlearning-client-py3-none-manylinux_2_24_x86_64.whl helper:/data
 sudo docker rm helper
