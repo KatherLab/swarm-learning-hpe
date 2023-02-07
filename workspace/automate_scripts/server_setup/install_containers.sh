bbc8c785722fbea6b9d83e2cbdf81057de979c53
Jeff <jiefu.zhu@tu-dresden.de>
2023-02-07 17:57:37 +0100

    update swop

--- workspace/automate_scripts/server_setup/install_containers.sh
+++ workspace/automate_scripts/server_setup/install_containers.sh
@@ -10,15 +10,15 @@ sudo docker login -u jiefu.zhu@tu-dresden.de -p hpe_eval hub.myenterpriselicense
 # sudo docker login -u kevin.pfeiffer@tu-dresden.de -p hpe_eval hub.myenterpriselicense.hpe.com
 
 echo "Download Swarm Network (SN) Node"
-sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/sn:1.1.0
+sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/sn:1.2.0
 
 echo "Download Swarm Learning (SL) Node"
-sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/sl:1.1.0
+sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/sl:1.2.0
 
 echo "Download Swarm Learning Command Interface (SWCI) Node"
-sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/swci:1.1.0
+sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/swci:1.2.0
 
 echo "Download Swarm Operator (SWOP) Node"
-sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/swop:1.1.0
+sudo docker pull hub.myenterpriselicense.hpe.com/hpe_eval/swarm-learning/swop:1.2.0
 
 sudo tar -xf $script_dir/license_and_softwares/HPE_SWARM_LEARNING_DOCS_EXAMPLES_SCRIPTS_Q2V41-11033.tar.gz -C /opt/hpe/swarm-learning-hpe/
