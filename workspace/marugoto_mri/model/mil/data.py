a87b13a2cab6353a727ab96dd910fb3abbad5dbc
Jeff <jiefu.zhu@tu-dresden.de>
2023-02-09 13:25:30 +0100

    initial update for marugoto mri

--- workspace/marugoto_mri/model/mil/data.py
+++ workspace/marugoto_mri/model/mil/data.py
@@ -1,19 +1,25 @@
 from dataclasses import dataclass
+from collections.abc import Sequence
+from typing import Any, Iterable, Optional, Tuple
 from pathlib import Path
-from typing import Any, Iterable, Optional, Sequence, Tuple, Union
 
 import h5py
+import numpy as np
+import numpy.typing as npt
 import pandas as pd
 import torch
-from marugoto.data import EncodedDataset, MapDataset, SKLearnEncoder
 from torch.utils.data import Dataset
 
-__all__ = ['BagDataset', 'make_dataset', 'get_cohort_df']
+from marugoto.data import EncodedDataset, MapDataset, SKLearnEncoder
+
+
+__all__ = ["BagDataset", "make_dataset", "get_cohort_df"]
 
 
 @dataclass
 class BagDataset(Dataset):
     """A dataset of bags of instances."""
+
     bags: Sequence[Iterable[Path]]
     """The `.h5` files containing the bags.
 
@@ -35,11 +41,11 @@ class BagDataset(Dataset):
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
         # collect all the features
-        feats = []
+        feat_list = []
         for bag_file in self.bags[index]:
-            with h5py.File(bag_file, 'r') as f:
-                feats.append(torch.from_numpy(f['feats'][:]))
-        feats = torch.concat(feats).float()
+            with h5py.File(bag_file, "r") as f:
+                feat_list.append(torch.from_numpy(f["feats"][:]))
+        feats = torch.concat(feat_list).float()
 
         # sample a subset, if required
         if self.bag_size:
@@ -48,30 +54,38 @@ class BagDataset(Dataset):
             return feats, len(feats)
 
 
-def _to_fixed_size_bag(bag: torch.Tensor, bag_size: int = 512) -> Tuple[torch.Tensor, int]:
+def _to_fixed_size_bag(
+    bag: torch.Tensor, bag_size: int = 32
+) -> Tuple[torch.Tensor, int]:
     # get up to bag_size elements
     bag_idxs = torch.randperm(bag.shape[0])[:bag_size]
     bag_samples = bag[bag_idxs]
 
     # zero-pad if we don't have enough samples
-    zero_padded = torch.cat((bag_samples,
-                             torch.zeros(bag_size - bag_samples.shape[0], bag_samples.shape[1])))
+    zero_padded = torch.cat(
+        (
+            bag_samples,
+            torch.zeros(bag_size - bag_samples.shape[0], bag_samples.shape[1]),
+        )
+    )
     return zero_padded, min(bag_size, len(bag))
 
 
 def make_dataset(
-        *,
-        bags: Sequence[Iterable[Path]],
-        targets: Tuple[SKLearnEncoder, Sequence[Any]],
-        add_features: Optional[Iterable[Tuple[Any, Sequence[Any]]]] = None,
-        bag_size: Optional[int] = None,
+    *,
+    bags: Sequence[Iterable[Path]],
+    targets: Tuple[SKLearnEncoder, npt.NDArray],
+    add_features: Optional[Iterable[Tuple[Any, npt.NDArray]]] = None,
+    bag_size: Optional[int] = None,
 ) -> MapDataset:
     if add_features:
         return _make_multi_input_dataset(
-            bags=bags, targets=targets, add_features=add_features, bag_size=bag_size)
+            bags=bags, targets=targets, add_features=add_features, bag_size=bag_size
+        )
     else:
         return _make_basic_dataset(
-            bags=bags, target_enc=targets[0], targs=targets[1], bag_size=bag_size)
+            bags=bags, target_enc=targets[0], targs=targets[1], bag_size=bag_size
+        )
 
 
 def get_target_enc(mil_learn):
@@ -79,14 +93,13 @@ def get_target_enc(mil_learn):
 
 
 def _make_basic_dataset(
-        *,
-        bags: Sequence[Iterable[Path]],
-        target_enc: SKLearnEncoder,
-        targs: Sequence[Any],
-        bag_size: Optional[int] = None,
+    *,
+    bags: Sequence[Iterable[Path]],
+    target_enc: SKLearnEncoder,
+    targs: npt.NDArray,
+    bag_size: Optional[int] = None,
 ) -> MapDataset:
-    assert len(bags) == len(targs), \
-        'number of bags and ground truths does not match!'
+    assert len(bags) == len(targs), "number of bags and ground truths does not match!"
 
     ds = MapDataset(
         zip_bag_targ,
@@ -107,27 +120,24 @@ def zip_bag_targ(bag, targets):
 
 
 def _make_multi_input_dataset(
-        *,
-        bags: Sequence[Iterable[Path]],
-        targets: Tuple[SKLearnEncoder, Sequence[Any]],
-        add_features: Iterable[Tuple[Any, Sequence[Any]]],
-        bag_size: Optional[int] = None
+    *,
+    bags: Sequence[Iterable[Path]],
+    targets: Tuple[SKLearnEncoder, npt.NDArray],
+    add_features: Iterable[Tuple[Any, npt.NDArray]],
+    bag_size: Optional[int] = None,
 ) -> MapDataset:
     target_enc, targs = targets
-    assert len(bags) == len(targs), \
-        'number of bags and ground truths does not match!'
+    assert len(bags) == len(targs), "number of bags and ground truths does not match!"  # type: ignore
     for i, (_, vals) in enumerate(add_features):
-        assert len(vals) == len(targs), \
-            f'number of additional attributes #{i} and ground truths does not match!'
+        assert len(vals) == len(
+            targs
+        ), f"number of additional attributes #{i} and ground truths does not match!"  # type: ignore
 
     bag_ds = BagDataset(bags, bag_size=bag_size)
 
     add_ds = MapDataset(
-        _splat_concat,
-        *[
-            EncodedDataset(enc, vals)
-            for enc, vals in add_features
-        ])
+        _splat_concat, *[EncodedDataset(enc, vals) for enc, vals in add_features]
+    )
 
     targ_ds = EncodedDataset(target_enc, targs)
 
@@ -141,41 +151,62 @@ def _make_multi_input_dataset(
     return ds
 
 
-def _splat_concat(*x): return torch.concat(x, dim=1)
+def _splat_concat(*x):
+    return torch.concat(x, dim=1)
 
 
 def _attach_add_to_bag_and_zip_with_targ(bag, add, targ):
     return (
-        torch.concat([
-            bag[0],  # the bag's features
-            add.repeat(bag[0].shape[0], 1)  # the additional features
-        ], dim=1),
+        torch.concat(
+            [
+                bag[0],  # the bag's features
+                add.repeat(bag[0].shape[0], 1),  # the additional features
+            ],
+            dim=1,
+        ),
         bag[1],  # the bag's length
         targ.squeeze(),  # the ground truth
     )
 
 
 def get_cohort_df(
-        clini_table: Union[Path, str], slide_csv: Union[Path, str], feature_dir: Union[Path, str],
-        target_label: str, categories: Iterable[str]
-) -> pd.DataFrame:
-    clini_df = pd.read_csv(clini_table, dtype=str) if Path(clini_table).suffix == '.csv' else pd.read_excel(clini_table,
-                                                                                                            dtype=str)
-    slide_df = pd.read_csv(slide_csv, dtype=str)
-    df = clini_df.merge(slide_df, on='PATIENT')
+    clini_table: Path,
+    slide_table: Path,
+    feature_dir: Path,
+    target_label: str,
+    categories: Optional[npt.NDArray[np.str_]],
+) -> Tuple[pd.DataFrame, npt.NDArray[np.str_]]:
+    clini_df = (
+        pd.read_csv(clini_table, dtype=str)
+        if Path(clini_table).suffix == ".csv"
+        else pd.read_excel(clini_table, dtype=str)
+    )
+    slide_df = pd.read_csv(slide_table, dtype=str)
+    df = clini_df.merge(slide_df, on="PATIENT")
+    assert not df.empty, "no overlap between clini and slide table."
+
+    # infer categories if not given
+    if categories is None:
+        categories = df.dropna(subset=target_label)[target_label].unique()
+    assert len(categories) >= 2, f"fewer than two categories found: {categories=}"
 
     # remove uninteresting
     df = df[df[target_label].isin(categories)]
     # remove slides we don't have
-    h5s = set(feature_dir.glob('*.h5'))
-    assert h5s, f'no features found in {feature_dir}!'
-    h5_df = pd.DataFrame(h5s, columns=['slide_path'])
-    h5_df['FILENAME'] = h5_df.slide_path.map(lambda p: p.stem)
-    df = df.merge(h5_df, on='FILENAME')
+    h5s = set(feature_dir.glob("*.h5"))
+    assert h5s, f"no features found in {feature_dir}!"
+    h5_df = pd.DataFrame(h5s, columns=["slide_path"])
+    h5_df["FILENAME"] = h5_df.slide_path.map(lambda p: p.stem)
+    df = df.merge(h5_df, on="FILENAME")
+    assert (
+        not df.empty
+    ), "clini and slide table match, but no corrensponding features have been found."
 
     # reduce to one row per patient with list of slides in `df['slide_path']`
-    patient_df = df.groupby('PATIENT').first().drop(columns='slide_path')
-    patient_slides = df.groupby('PATIENT').slide_path.apply(list)
-    df = patient_df.merge(patient_slides, left_on='PATIENT', right_index=True).reset_index()
+    patient_df = df.groupby("PATIENT").first().drop(columns="slide_path")
+    patient_slides = df.groupby("PATIENT").slide_path.apply(list)
+    df = patient_df.merge(
+        patient_slides, left_on="PATIENT", right_index=True
+    ).reset_index()
 
-    return df
+    return df, np.array(categories)
