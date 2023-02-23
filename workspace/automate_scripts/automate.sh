bca64397232d8378b530ce8b57724d670525b3f0
Jeff <jiefu.zhu@tu-dresden.de>
2023-02-23 15:20:25 +0100

    update path in automate.sh

--- workspace/automate_scripts/automate.sh
+++ workspace/automate_scripts/automate.sh
@@ -61,11 +61,11 @@ do
 done
 
 if [ $ACTION = prerequisite ]; then
-  sh .workspace/automate_scripts/server_setup/test_open_exposed_ports.sh
-  sh .workspace/automate_scripts/server_setup/prerequisites.sh
+  sh ./workspace/automate_scripts/server_setup/test_open_exposed_ports.sh
+  sh ./workspace/automate_scripts/server_setup/prerequisites.sh
   if [ $ip_addr = $sentinal_ip ]
     then
-    sh .workspace/automate_scripts/server_setup/install_apls.sh
+    sh ./workspace/automate_scripts/server_setup/install_apls.sh
   fi
 fi
 
@@ -75,12 +75,12 @@ if [ $ACTION = server_setup ]; then
        echo "Some or all of the parameters are empty";
        help
   fi
-  sh .workspace/automate_scripts/server_setup/install_containers.sh
-  sh .workspace/automate_scripts/server_setup/gpu_env_setup.sh
-  sh .workspace/automate_scripts/sl_env_setup/gen_cert.sh -w "$workspace_name"
+  sh ./workspace/automate_scripts/server_setup/install_containers.sh
+  sh ./workspace/automate_scripts/server_setup/gpu_env_setup.sh
+  sh ./workspace/automate_scripts/sl_env_setup/gen_cert.sh -w "$workspace_name"
   if [ $ip_addr != $sentinal_ip ]
     then
-    sh .workspace/automate_scripts/sl_env_setup/get_dataset.sh -w "$workspace_name" -s $sentinal_ip
+    sh ./workspace/automate_scripts/sl_env_setup/get_dataset.sh -w "$workspace_name" -s $sentinal_ip
   fi
 fi
 
@@ -99,16 +99,16 @@ if [ $ACTION = final_setup ]; then
      for value in $host_ip
         do
          echo sharing certificate with ip address $value
-         sh .workspace/automate_scripts/sl_env_setup/share_cert.sh -t "$value" -w "$workspace_name"
+         sh ./workspace/automate_scripts/sl_env_setup/share_cert.sh -t "$value" -w "$workspace_name"
         done
-     sh .workspace/automate_scripts/sl_env_setup/license_server_fix.sh
+     sh ./workspace/automate_scripts/sl_env_setup/license_server_fix.sh
   else
      echo "This host is not a sentinal node and will not be used for initiating the cluster, only as swarm network node"
      sn_command="--sentinel-ip=$sentinal_ip"
-     #sh .workspace/automate_scripts/sl_env_setup/share_cert.sh -t "$sentinal_ip" -w "$workspace_name"
+     #sh ./workspace/automate_scripts/sl_env_setup/share_cert.sh -t "$sentinal_ip" -w "$workspace_name"
 
   fi
-  sh .workspace/automate_scripts/sl_env_setup/replacement.sh -w "$workspace_name" -s "$sentinal_ip" -n "$num_peers" -e "$num_epochs"
-  sh .workspace/automate_scripts/sl_env_setup/setup_sl-cli-lib.sh -w "$workspace_name"
+  sh ./workspace/automate_scripts/sl_env_setup/replacement.sh -w "$workspace_name" -s "$sentinal_ip" -n "$num_peers" -e "$num_epochs"
+  sh ./workspace/automate_scripts/sl_env_setup/setup_sl-cli-lib.sh -w "$workspace_name"
 
 fi
