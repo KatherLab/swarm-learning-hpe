eb1be83fa383f93c95467ee200614170195ad115
Jeff <jiefu.zhu@tu-dresden.de>
2023-02-24 11:41:24 +0100

    add switch to vpn generation

--- workspace/automate_scripts/server_setup/setup_vpntunnel.sh
+++ workspace/automate_scripts/server_setup/setup_vpntunnel.sh
@@ -19,7 +19,7 @@ do
 done
 
 sudo apt-get -y install openvpn
-if sudo ls /etc/openvpn/credentials >/dev/null 2>&1; then
+if ! sudo ls /etc/openvpn/credentials >/dev/null 2>&1; then
     # The file exists, continue with the following commands
     sudo touch /etc/openvpn/credentials
     sudo printf '%s\n' 'jeffzhu6969@gmail.com' '5885ohdude' > /etc/openvpn/credentials
