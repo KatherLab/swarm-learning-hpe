a36856efff3eb2be22152cca0ecc0e2e62cf8ab1
Jeff <jiefu.zhu@tu-dresden.de>
2023-03-07 13:04:33 +0100

    update pre

--- workspace/automate_scripts/server_setup/prerequisites.sh
+++ workspace/automate_scripts/server_setup/prerequisites.sh
@@ -27,7 +27,7 @@ done
 
 echo "Installing required packages"
 sudo apt-get update
-sudo apt-get purge container.io -y
+#sudo apt-get purge container.io -y
 sudo apt-get install -y docker.io curl git openssh-server openssh-client
 sudo systemctl start docker
 # Update package manager and install pip
