84e7bb0250550282e4f31aea8f6f1429d0b1d31a
Jeff <jiefu.zhu@tu-dresden.de>
2023-03-08 15:51:51 +0100

    update logs

--- workspace/automate_scripts/server_setup/test_dockersetup.sh
+++ workspace/automate_scripts/server_setup/test_dockersetup.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+set -eu
+
 echo "Testing Docker installation"
 groupadd -f docker
 sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world
+
+if [ $? -ne 0 ]; then
+    echo "An error occurred while running the script. Please check the output above for more details."
+    exit 1
+fi
+
+echo "Docker installation successful."
