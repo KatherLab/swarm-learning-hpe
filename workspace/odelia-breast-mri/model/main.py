019868df0f1cc4bfd69d317fb6841110a031267a
Jeff <jiefu.zhu@tu-dresden.de>
2023-03-10 16:15:48 +0100

    delete comments

--- workspace/odelia-breast-mri/model/main.py
+++ workspace/odelia-breast-mri/model/main.py
@@ -43,7 +43,6 @@ def cal_weightage(train_size):
     return int(100 * train_size / full_dataset_size)
 
 if __name__ == "__main__":
-    # ------------ Settings/Defaults ----------------
     task_data_name = 'WP1'
     scratchDir = os.getenv('SCRATCH_DIR', '/platform/scratch')
     dataDir = os.getenv('DATA_DIR', '/platform/data/')
@@ -54,18 +53,12 @@ if __name__ == "__main__":
     useAdaptiveSync = bool(os.getenv('USE_ADAPTIVE_SYNC', False))
     syncFrequency = int(os.getenv('SYNC_FREQUENCY', 512))
 
-    #print(os.getenv('DATA_DIR'))
-    #print(f"Using {scratchDir} for training")
     current_time = datetime.now().strftime("%Y_%m_%d_%H%M%S")
     path_run_dir = os.path.join(scratchDir, (str(current_time)+ '_' +task_data_name + 'swarm_learning'))  # !
-    # path_run_dir = Path.cwd() / 'runs' / str(current_time)
-    #path_run_dir.mkdir(parents=True, exist_ok=True)
+
     accelerator = 'gpu' if torch.cuda.is_available() else 'cpu'
     print(f"Using {accelerator} for training")
 
-    # ------------ Load Data ----------------
-    #dataDir = os.getenv('DATA_DIR', '/tmp/test/host1-partial-data')  # !
-    #print current directory
     print("Current Directory " , os.getcwd())
     ds = DUKE_Dataset3D(
         flip=True,
@@ -75,13 +68,9 @@ if __name__ == "__main__":
     val_size = int(0.2 * len(ds))
     ds_train = Subset(ds, list(range(train_size)))
     ds_val = Subset(ds, list(range(train_size, train_size+val_size)))
-    #ds_test = Subset(ds, list(range(train_size + val_size, len(ds))))
     print('train_size: ',train_size)
     print('val_size: ',val_size)
-    #print('test_size: ',ds_train)
-    #print(ds_train[0])
-    #print(ds_val[0])
-    #print(ds_test[0])
+
 
     dm = DataModule(
         ds_train = ds_train,
@@ -119,9 +108,7 @@ if __name__ == "__main__":
         torch.autograd.set_detect_anomaly(True)
         trainer = Trainer(
             accelerator=accelerator,
-            # devices=[0],
             precision=16,
-            # gradient_clip_val=0.5,
             default_root_dir=str(path_run_dir),
             callbacks=[checkpointing],  # early_stopping
             enable_checkpointing=True,
@@ -129,7 +116,6 @@ if __name__ == "__main__":
             min_epochs=5,
             log_every_n_steps=log_every_n_steps,
             auto_lr_find=False,
-            # limit_val_batches=0, # 0 = disable validation - Note: Early Stopping no longer available
             max_epochs=max_epochs,
             num_sanity_val_steps=2,
             logger=TensorBoardLogger(save_dir=path_run_dir)
@@ -164,7 +150,7 @@ if __name__ == "__main__":
             logger=TensorBoardLogger(save_dir=path_run_dir)
         )
         trainer.fit(model, datamodule=dm)
-        swarmCallback.on_train_end()  # !
+        swarmCallback.on_train_end()
     model.save_best_checkpoint(trainer.logger.log_dir, checkpointing.best_model_path)
 
     predict(path_run_dir, os.path.join(dataDir, task_data_name,'test'))
