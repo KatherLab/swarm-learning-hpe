bd2d41125f7198d23d4112f46106a8d30d4c61ae
Jeff <jiefu.zhu@tu-dresden.de>
2023-03-10 16:06:17 +0100

    update swops

--- workspace/automate_scripts/launch_sl/run_sl.sh
+++ workspace/automate_scripts/launch_sl/run_sl.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 # Get the directory containing this script
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-# Remove any stopped containers
-docker rm $(docker ps --filter status=exited -q) || true
-
 # Define a help function
 show_help() {
   echo "Usage: $(basename "$0") [-w WORKSPACE]"
@@ -16,25 +13,42 @@ show_help() {
   echo ""
   echo "Options:"
   echo "  -w WORKSPACE    The name of the workspace directory to use (default: none)"
+  echo "  -r              Remove any stopped containers"
   echo "  -h              Show this help message"
   echo ""
   exit 0
 }
 
+# Set default values for options
+workspace=""
+remove_stopped_containers=false
+
 # Process command line options
-while getopts ":w:h" opt; do
+while getopts "r:w:h" opt; do
   case ${opt} in
+    r ) remove_stopped_containers=true ;; # Remove any stopped containers
     w ) workspace=${OPTARG} ;;
     h ) show_help ;;
     \? ) show_help ;;
     : ) echo "Option -$OPTARG requires an argument." >&2; exit 1 ;;
   esac
 done
+
 ip_addr=$(ip addr show tun0 2>/dev/null | grep 'inet ' | awk '{print $2}' | cut -f1 -d'/')
 
 if [[ -z "$ip_addr" ]]; then
     echo "Error: tun0 interface not found. Please connect to the VPN first. Use script setup_vpntunnel.sh"
     exit 1
+fi
+
+# Set the Docker image based on the workspace
+if [[ "$workspace" == "marugoto_mri" ]]; then
+  ml_image="user-env-marugoto-swop"
+elif [[ "$workspace" == "odelia-breast-mri" ]]; then
+  ml_image="user-env-pyt1.13-swop"
+else
+  echo "Error: invalid workspace specified."
+  exit 1
 fi
 
 # Launch the swarm learning container with the specified options
@@ -46,16 +60,23 @@ fi
   --sl-fs-port=16000 \
   --key=/opt/hpe/swarm-learning-hpe/cert/sl-TUD-key.pem \
   --cert=/opt/hpe/swarm-learning-hpe/cert/sl-TUD-cert.pem \
-  --capath=/cert/ca/capath \
+  --capath=/opt/hpe/swarm-learning-hpe/cert/ca/capath \
   --ml-it \
-  --ml-image=swarm \
+  --ml-image="$ml_image" \
   --ml-name=ml1 \
   --ml-w=/tmp/test \
   --ml-entrypoint=python3 \
   --ml-cmd=model/main.py \
   --ml-v=workspace/"$workspace"/model:/tmp/test/model \
+  --ml-v=workspace/"$workspace"/user/data-and-scratch/data:/platform/data \
   --ml-e MODEL_DIR=model \
   --ml-e MAX_EPOCHS=5 \
   --ml-e MIN_PEERS=2 \
   --ml-e https_proxy= \
+  --ml-user 0:0 \
   --apls-ip="$ip_addr"
+
+# Remove any stopped containers if specified
+if [ "$remove_stopped_containers" = true ]; then
+  docker rm "$(docker ps --filter status=exited -q)" || true
+fi
