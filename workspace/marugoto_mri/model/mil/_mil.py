3406463f25de6a5611fc65b6d9328bf956fce54d
Jeff <jiefu.zhu@tu-dresden.de>
2023-03-12 16:46:28 +0100

    update callback

--- workspace/marugoto_mri/model/mil/_mil.py
+++ workspace/marugoto_mri/model/mil/_mil.py
@@ -128,16 +128,18 @@ def train(
     loss_func = nn.CrossEntropyLoss(weight=weight)
 
     dls = DataLoaders(train_dl, valid_dl)
-    swarmCallback = SwarmCallback(syncFrequency=syncFrequency,
-                                  minPeers=min_peers,
-                                  maxPeers=max_peers,
-                                  adsValData=valid_ds,
-                                  adsValBatchSize=2,
-                                  nodeWeightage=cal_weightage(len(train_ds)),
-                                  model=model)
-    swarmCallback.logger.setLevel(logging.DEBUG)
+
 
     if local_compare_flag:
+        swarmCallback = SwarmCallback(syncFrequency=syncFrequency,
+                                      minPeers=min_peers,
+                                      maxPeers=max_peers,
+                                      adsValData=valid_ds,
+                                      adsValBatchSize=2,
+                                      nodeWeightage=cal_weightage(len(train_ds)),
+                                      model=model,
+                                      checkinModelOnTrainEnd='inactive',)
+        swarmCallback.logger.setLevel(logging.DEBUG)
         print('local compare flag is set')
         learn = Learner(dls, model, loss_func=loss_func, metrics=[RocAuc()], path=path)
         cbs = [
@@ -146,7 +148,14 @@ def train(
         ]
         learn.fit_one_cycle(n_epoch=n_epoch, lr_max=1e-4, cbs=cbs)
     else:
-
+        swarmCallback = SwarmCallback(syncFrequency=syncFrequency,
+                                      minPeers=min_peers,
+                                      maxPeers=max_peers,
+                                      adsValData=valid_ds,
+                                      adsValBatchSize=2,
+                                      nodeWeightage=cal_weightage(len(train_ds)),
+                                      model=model)
+        swarmCallback.logger.setLevel(logging.DEBUG)
         print('local compare flag is not set')
         swarmCallback.on_train_begin()
         learn = Learner(dls, model, loss_func=loss_func, metrics=[RocAuc()], path=path)
