8450affda7eeffd5e5611dcc4b9c06dea45fe951
pfeiffer-dev <84971615+kevinxpfeiffer@users.noreply.github.com>
2023-03-23 16:09:26 +0100

    updated dockerfile

--- workspace/tabular-breast/__init__.py
+++ workspace/tabular-breast/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-__author__ = "Jeff"
+__author__ = "Kevin"
 __copyright__ = "Copyright 2023, Kather Lab"
 __license__ = "MIT"
 __version__ = "0.1.0"
-__maintainer__ = ["Jeff"]
-__email__ = "jiefu.zhu@tu-dresden.de"
+__maintainer__ = ["Kevin"]
+__email__ = "kevin.pfeiffer@tu-dresden.de"
