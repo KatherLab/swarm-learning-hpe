7f17f7c9cbb4b4871518c7f9224b154bc90191fa
Jeff <jiefu.zhu@tu-dresden.de>
2023-04-07 11:20:50 +0200

    update last checkpoint

--- workspace/odelia-breast-mri/model/models/base_model.py
+++ workspace/odelia-breast-mri/model/models/base_model.py
@@ -56,8 +56,10 @@ class VeryBasicModel(pl.LightningModule):
     def save_best_checkpoint(cls, path_checkpoint_dir, best_model_path):
         with open(Path(path_checkpoint_dir) / 'best_checkpoint.json', 'w') as f:
             json.dump({'best_model_epoch': Path(best_model_path).name}, f)
+    @classmethod
+    def save_last_checkpoint(cls, path_checkpoint_dir, best_model_path):
+        with open(Path(path_checkpoint_dir) / 'last_checkpoint.json', 'w') as f:
             json.dump({'last_model_epoch': "last.ckpt"}, f)
-
     @classmethod
     def _get_best_checkpoint_path(cls, path_checkpoint_dir, version=0, **kwargs):
         path_version = 'lightning_logs/version_' + str(version)
@@ -67,7 +69,7 @@ class VeryBasicModel(pl.LightningModule):
     @classmethod
     def _get_last_checkpoint_path(cls, path_checkpoint_dir, version=0, **kwargs):
         path_version = 'lightning_logs/version_' + str(version)
-        with open(Path(path_checkpoint_dir) / path_version / 'best_checkpoint.json', 'r') as f:
+        with open(Path(path_checkpoint_dir) / path_version / 'last_checkpoint.json', 'r') as f:
             path_rel_last_checkpoint = Path(json.load(f)['last_model_epoch'])
         return Path(path_checkpoint_dir) / path_rel_last_checkpoint
     @classmethod
