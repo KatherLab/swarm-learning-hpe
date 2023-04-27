10e81175c73c40c7bc63d5730fba27932f011ece
pfeiffer-dev <84971615+kevinxpfeiffer@users.noreply.github.com>
2023-04-27 19:13:58 +0200

    Update main.py

--- workspace/single-cell/model/main.py
+++ workspace/single-cell/model/main.py
@@ -136,10 +136,10 @@ def loadData(dataDir, device):
 
 def main():
     # Set parameters and directories
-    batchSz = 50
+    batchSz = 5
     default_max_epochs = 5
     default_min_peers = 2 
-    default_syncFrequency = 2
+    default_syncFrequency = 5
     default_useAdaptiveSync = False
     
     dataDir = os.getenv('DATA_DIR', '/platform/data')
