92f68391f8c9d62d89f87846428422b3de52eb75
pfeiffer-dev <84971615+kevinxpfeiffer@users.noreply.github.com>
2023-06-14 16:59:25 +0200

    Updated workspace

--- workspace/odelia-breast-mri/model/models/__init__.py
+++ workspace/odelia-breast-mri/model/models/__init__.py
@@ -1,2 +1,6 @@
 from .base_model import VeryBasicModel, BasicModel, BasicClassifier
 from .resnet import ResNet
+from .efficientNet import EfficientNet, EfficientNet3D, EfficientNet3Db7
+from .vit import VisionTransformer
+from .densenet import DenseNet121
+from .uNet3D import UNet3D
--- workspace/odelia-breast-mri/model/models/densenet.py
+++ workspace/odelia-breast-mri/model/models/densenet.py
@@ -1,19 +1,14 @@
 from .base_model import BasicClassifier
 import monai.networks.nets as nets
 import torch
+import torch.nn.functional as F
 
-
-class ResNet(BasicClassifier):
+class DenseNet121(BasicClassifier):
     def __init__(
             self,
             in_ch,
             out_ch,
             spatial_dims=3,
-            block='basic',  # 'basic', 'bottleneck'
-            layers=None,
-            # layers=[3, 4, 23, 3], # for Resnet 101
-            block_inplanes=[64, 128, 256, 512],
-            feed_forward=True,
             loss=torch.nn.BCEWithLogitsLoss,
             loss_kwargs={},
             optimizer=torch.optim.AdamW,
@@ -25,8 +20,7 @@ class ResNet(BasicClassifier):
     ):
         super().__init__(in_ch, out_ch, spatial_dims, loss, loss_kwargs, optimizer, optimizer_kwargs, lr_scheduler,
                          lr_scheduler_kwargs, aucroc_kwargs, acc_kwargs)
-        self.model = nets.ResNet(block, layers, block_inplanes, spatial_dims, in_ch, 7, 1, False, 'B', 1.0, out_ch,
-                                 feed_forward, True)
+        self.model = nets.DenseNet264(spatial_dims=spatial_dims, in_channels=in_ch, out_channels=out_ch)
 
     def forward(self, x_in, **kwargs):
         pred_hor = self.model(x_in)
