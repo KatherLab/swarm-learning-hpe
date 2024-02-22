2792ba26c6e4cb433de8082f6f32ebe62acf8cec
Jeff <jiefu.zhu@tu-dresden.de>
2024-02-22 13:38:04 +0100

    improve alias

--- workspace/automate_scripts/sl_env_setup/add_alias.sh
+++ workspace/automate_scripts/sl_env_setup/add_alias.sh
@@ -1,41 +1,45 @@
-#!/bin/bash
-#set -eu
+#!/bin/sh
+# set -eu
+host_index=""
 
 # Help function
-help()
-{
+help() {
    echo ""
    echo "Generate alias for swarm learning scripts"
-   echo ""
-   echo "-h               Display this help message."
+   echo "-d    The host index, choose from [TUD, Ribera, VHIO, Radboud, UKA, Utrecht, Mitera, Cambridge, Zurich] for your site"
+   echo "-h    Display this help message."
    exit 1
 }
 
 # Process command options
-while getopts "i:h" opt
-do
+while getopts "d:h" opt; do
    case "$opt" in
       h ) help ;;
+      d ) host_index="$OPTARG" ;;
       * ) help ;;
    esac
 done
 
-alias runsn='sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_sn.sh'
-alias runswop='sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_swop.sh'
-alias runswci='sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_swci.sh'
-alias runsl='sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_sl.sh'
-alias cklog='sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/check_latest_log.sh'
-alias stophpe='sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/stopswarm.sh'
-
-# if bash then run source ~/.bashrc
-. ~/.bashrc
-# if zsh then run source ~/.zshrc
-#source ~/.zshrc
-
-
-# If an error occurs, print an error message and exit
-if [ $? -ne 0 ]; then
-    echo "An error occurred while running the script. Please check the output above for more details."
-    exit 1
+# Check required options are set
+if [ -z "$host_index" ]; then
+   echo "Error: missing host index"
+   help
 fi
-echo "alias generated successfully."
+
+# Define aliases
+alias runsn="sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_sn.sh -d $host_index"
+alias runswop="sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_swop.sh -d $host_index"
+alias runswci="sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_swci.sh -d $host_index"
+alias runsl="sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/run_sl.sh -d $host_index"
+alias cklog="sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/check_latest_log.sh -d $host_index"
+alias stophpe="sudo sh /opt/hpe/swarm-learning-hpe/workspace/automate_scripts/launch_sl/stopswarm.sh -d $host_index"
+
+# Inform user about manual sourcing
+echo "Aliases generated successfully. Please source your shell configuration file (e.g., ~/.bashrc or ~/.zshrc) to apply changes."
+
+# Commented out the automatic sourcing and error check
+# . ~/.bashrc
+# if [ $? -ne 0 ]; then
+#     echo "An error occurred while running the script. Please check the output above for more details."
+#     exit 1
+# fi
--- workspace/automate_scripts/sl_env_setup/setup_aliases.sh
+++ workspace/automate_scripts/sl_env_setup/setup_aliases.sh
@@ -1,24 +1,50 @@
-#!/bin/bash
+#!/bin/sh
+host_index=""
+
+# Help function
+help() {
+  echo ""
+  echo "Generate alias for swarm learning scripts"
+  echo "-d    The host index, choose from [TUD, Ribera, VHIO, Radboud, UKA, Utrecht, Mitera, Cambridge, Zurich] for your site"
+  echo "-h    Display this help message."
+  exit 1
+}
+
+# Process command options
+while getopts "d:h" opt; do
+  case "$opt" in
+    h ) help ;;
+    d ) host_index="$OPTARG" ;;
+    * ) help ;;
+  esac
+done
+
+# Check required options are set
+if [ -z "$host_index" ]; then
+  echo "Error: missing host index"
+  help
+fi
 
 # Path to your alias script
-alias_script="/opt/hpe/swarm-learning-hpe/workspace/automate_scripts/sl_env_setup/add_alias.sh"
+alias_script="/opt/hpe/swarm-learning-hpe/workspace/automate_scripts/sl_env_setup/add_alias.sh -d $host_index"
 
 # Determine which shell the user is using and set the config file path accordingly
-if [[ $SHELL == */bash ]]; then
-    config_file="$HOME/.bashrc"
-elif [[ $SHELL == */zsh ]]; then
-    config_file="$HOME/.zshrc"
-else
+config_file=""
+case "$SHELL" in
+  */bash) config_file="$HOME/.bashrc" ;;
+  */zsh) config_file="$HOME/.zshrc" ;;
+  *)
     echo "Unsupported shell. This script supports bash and zsh only."
     exit 1
-fi
+    ;;
+esac
 
 # Check if the alias script is already sourced in the config file
 if grep -q "$alias_script" "$config_file"; then
-    echo "Alias script already sourced in $config_file"
+  echo "Alias script already sourced in $config_file"
 else
-    # If not, append the source command to the config file
-    echo "Sourcing alias script in $config_file..."
-    echo "source $alias_script" >> "$config_file"
-    echo "Alias script sourced successfully. Please restart your terminal or source the $config_file file."
+  # If not, append the source command to the config file
+  echo "Sourcing alias script in $config_file..."
+  echo "source $alias_script" >> "$config_file"
+  echo "Alias script sourced successfully. Please restart your terminal or source the $config_file file."
 fi
