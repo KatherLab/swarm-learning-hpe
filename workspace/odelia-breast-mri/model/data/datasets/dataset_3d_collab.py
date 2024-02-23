3e125f275ceb24a9acac60331d4fc281509c5149
Jeff <jiefu.zhu@tu-dresden.de>
2024-02-23 18:25:31 +0100

    rollback for cambridge

--- workspace/odelia-breast-mri/model/data/datasets/dataset_3d_collab.py
+++ workspace/odelia-breast-mri/model/data/datasets/dataset_3d_collab.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import pandas as pd 
+import pandas as pd
 from data.datasets import SimpleDataset3D
 
 
@@ -9,7 +9,7 @@ class DUKE_Dataset3D_collab(SimpleDataset3D):
         if item_pointers is None:
             item_pointers = []
         super().__init__(path_root, item_pointers, crawler_glob, transform, image_resize, flip, image_crop, norm, to_tensor)
-        df = pd.read_csv(self.path_root.parent/'datasheet.csv')#, header=[0, 2])
+        df = pd.read_csv(self.path_root.parent/'cambridge.csv')#, header=[0, 2])
         #print('******')
         #print('self.path_root.parent', self.path_root.parent)
         #df = df[df[df.columns[38]] == 0] # check if cancer is bilateral=1, unilateral=0 or NC
@@ -24,40 +24,26 @@ class DUKE_Dataset3D_collab(SimpleDataset3D):
         #print('Data_____frame')
         #print(self.df)
 
-    from pathlib import Path
 
     def __getitem__(self, index):
         uid = self.item_pointers[index]
-        # Directory path for the specific item
-        item_dir = self.path_root / uid
-        # Fetch all .nii.gz files in the directory
-        nii_gz_files = list(item_dir.glob('**/*.nii.gz'))
-        # Default file selection
-        file_name = 'SUB_4.nii.gz'
-        # If there are multiple .nii.gz files, prioritize SUB_4.nii.gz if it exists
-        if len(nii_gz_files) > 1:
-            sub_4_path = item_dir / file_name
-            if sub_4_path in nii_gz_files:
-                path_item = sub_4_path
-            else:
-                # If SUB_4.nii.gz is not found, just use the first file (or any other logic)
-                path_item = nii_gz_files[0]
-        elif nii_gz_files:
-            # If there's only one .nii.gz file, use it
-            path_item = nii_gz_files[0]
-        else:
-            # Handle the case where no .nii.gz files are found
-            raise FileNotFoundError(f"No .nii.gz files found in {item_dir}")
-
+        #print(uid)
+        #path_item = [self.path_root/uid/name for name in [ '**.nii.gz' ]]
+        file_name = uid + '.nii.gz'
+        #file_name = 'Sub.nii.gz' # For Aachen dataset
+        path_item = Path.joinpath(self.path_root/uid, file_name)
+        #print(path_item)
         img = self.load_item(path_item)
+        #print(img)
         target = self.df.loc[uid]['Malign']
-        return {'uid': uid, 'source': self.transform(img), 'target': target}
+        #print(target)
+        return {'uid':uid, 'source': self.transform(img), 'target':target}
 
     @classmethod
     def run_item_crawler(cls, path_root, crawler_ext, **kwargs):
         return [path.relative_to(path_root).name for path in Path(path_root).iterdir() if path.is_dir() ]
 
-    
+
     def get_labels(self):
         # Assuming 'Malign' is a column in self.df after combining datasets
         # This method should return a list or array of labels corresponding to each item in the dataset
