5b4074cee045c24c475b00c2ec44cb6c2afd0f66
Kevin Pfeiffer <84971615+pfeifferis@users.noreply.github.com>
2025-10-24 14:08:00 +0200

    Update data file path to use renorm version
    Signed-off-by: Kevin Pfeiffer <84971615+pfeifferis@users.noreply.github.com>

--- workspace/single-cell/model/main.py
+++ workspace/single-cell/model/main.py
@@ -96,7 +96,7 @@ def loadData(dataDir, experiment, organ, label):
     """
 
     # Load data
-    data_path = glob.glob(os.path.join(dataDir, organ+'_clean_subset_4_union_hvgs_v2.h5ad'))
+    data_path = glob.glob(os.path.join(dataDir, organ+'_clean_subset_4_union_hvgs_renorm.h5ad'))
     
     if not data_path:
         raise FileNotFoundError(
